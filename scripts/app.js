20be9a161d08523f960f301e3fde8e14f1761512
Negishi <takuto.negishi@gmail.com>
2020-05-06 22:56:02 +0900

    クリアー時のイベント実装

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,12 @@
 
 var canvasWidth = window.innerWidth*0.9 - 20,
     canvasHeight = window.innerHeight*0.9 - 120, // キャンバス高さ
-    numOfObject = Math.round(Math.random() * 7) + 3, // boxの数。3～10でランダム
+    numOfBox = Math.round(Math.random() * 7) + 3, // boxの数。3～10でランダム
     boxHeight = 64; // boxデフォルトサイズ(scale = 1)
-console.log(canvasHeight, numOfObject)
+
 // boxの数の+1個が収まるスケールにする
-var scale = canvasHeight / (boxHeight * (numOfObject + 1));
+var scale = canvasHeight / (boxHeight * (numOfBox + 1)),
+    boxRealSize = boxHeight*scale;
 
 // Matter.jsのAPIの読み込み
 var Engine = Matter.Engine,
@@ -18,7 +19,6 @@ var Engine = Matter.Engine,
 
 var engine = Engine.create();
 
-// create a renderer
 var render = Render.create({
       element: document.getElementById("canvas"), //Canvasを設置する要素を指定
       engine: engine, //生成したEngineを指定
@@ -32,7 +32,7 @@ var render = Render.create({
     });
 
 var mousedrag = MouseConstraint.create(engine, {
-  element: canvas.childNodes[0], //マウス操作を感知する要素を指定（DEMOでは生成したcanvasを指定）
+  element: canvas.childNodes[0], //マウス操作を感知する要素
   constraint: {
     render: {
       strokeStyle: "rgba(0, 0, 0, 0)" //マウス操作の表示を隠す
@@ -41,11 +41,11 @@ var mousedrag = MouseConstraint.create(engine, {
 });
 World.add(engine.world, mousedrag);
 
-//四角の要素
+// 四角の要素
 var boxes = [];
-for(var i = 1; i <= numOfObject; i++) {
+for(var i = 1; i <= numOfBox; i++) {
   var obj = Bodies.rectangle(
-    Math.random() * (canvasWidth) + 0, // x位置
+    Math.random() * canvasWidth, // x位置
     Math.random() * (canvasHeight/3) + canvasHeight/3*2, // y位置
     64*scale,
     64*scale,
@@ -60,7 +60,7 @@ for(var i = 1; i <= numOfObject; i++) {
       friction: 1, // 摩擦
       render: { //ボールのレンダリングの設定
 				sprite: { //スプライトの設定
-          texture: './images/tex/' + i + '.png', //スプライトに使うテクスチャ画像を指定
+          texture: './images/tex/' + i + '.png', // テクスチャ画像
           xScale: scale,
           yScale: scale
 				}
@@ -80,16 +80,13 @@ borders.push(Bodies.rectangle(0, canvasHeight, canvasWidth*2, 1, { isStatic: tru
 borders.push(Bodies.rectangle(0, 0, 1, canvasHeight*2, { isStatic: true }));
 borders.push(Bodies.rectangle(canvasWidth, 0, 1, canvasHeight*2, { isStatic: true }));
  
-// add all of the bodies to the world
 World.add(engine.world, borders);
 
 Events.on(mousedrag, "startdrag", function(e) {
-  //console.log("startdrag", mousedrag.body);
   console.log("startdrag", mousedrag.body.angle);
-  Body.rotate( mousedrag.body, Math.PI*2 - mousedrag.body.angle);
-  //if(e.body.label == "ne") {
-  //  console.log(e);
-  //}
+  if (!mousedrag.body.isStatic) {
+    Body.rotate( mousedrag.body, Math.PI*2 - mousedrag.body.angle);
+  }
 });
 Events.on(mousedrag, "enddrag", function(e) {
   console.log("enddrag");
@@ -98,24 +95,26 @@ Events.on(mousedrag, "enddrag", function(e) {
 
 Engine.run(engine);
  
-// run the renderer
 Render.run(render);
 
 
-var btnReload = document.getElementById("btnCheck");
-btnReload.addEventListener('click', function() {
+var btnCheck = document.getElementById("btnCheck");
+btnCheck.addEventListener('click', function() {
   var isClear = true;
   for (var i = 0 ;  i < boxes.length-1 ; i++) {
     console.log(boxes[i].label, boxes[i].position.y, boxes[i+1].position.y)
     // NGなケース：次のboxの位置関係で判定
-    if (boxes[i].position.y < boxes[i+1].position.y + boxHeight*scale*0.9) {
-      antoast.warn(boxes[i+1].label + 'を、' + boxes[i].label + 'の　うえにのせよう');
+    if (boxes[i].position.y < boxes[i+1].position.y + boxRealSize*0.9) {
+      antoast.warn(boxes[i].label + 'のうえに' + boxes[i+1].label + 'をのせよう');
       isClear = false;
       break;
     }
   }
   if (isClear) {
-    antoast.success('すごーい！　やったね！');
+    btnCheck.style.display = 'none';
+    antoast.success('すごーい！ やったね！');
+
+    completeAnim();
   }
 }, false);
 
@@ -125,6 +124,48 @@ btnReload.addEventListener('click', function() {
 }, false);
 
 
+function completeAnim(fx, tx, fy, ty) {
+  
+  for (var i = 0 ;  i < boxes.length ; i++) {
+    boxes[i].isStatic = true;
+  }
+
+  var getCubePos = function() {
+    var p = {x: Math.random() * canvasWidth,
+             y: Math.random() * canvasHeight};
+    if (fx < p.x && tx > p.x && fy < p.y && ty > p.y){
+      p = getCubePos(fx, tx, fy, ty);
+    }
+    return p;
+  }
+  var objs = [];
+  var fx = boxes[0].position.x-boxRealSize*0.5 - 5,
+      tx = boxes[0].position.x+boxRealSize*0.5 + 5,
+      fy = canvasHeight - boxRealSize*numOfBox - 5,
+      ty = canvasHeight;
+  for(var i = 1; i <= 200; i++) {
+    var p = getCubePos(fx, tx, fy, ty);
+    var obj = Bodies.rectangle(
+      p.x, // x位置
+      p.y, // y位置
+      15,
+      15,
+      { 
+        chamfer: 0, //角丸
+        density: 3, // 密度
+        restitution: 0.5, //反発
+        frictionAir: 0.1, // 空気抵抗
+        friction: 1, // 摩擦
+        render: { //ボールのレンダリングの設定
+        },
+      }
+    );
+    objs.push(obj);
+  }
+
+  World.add(engine.world, objs);
+}
+
 var antoast = new anToast();
 antoast.setOption(5000, 'top');
-antoast.success('したから　ちいさいじゅんにつんでいこう');
+antoast.success('したから ちいさいじゅんに つんでみよう');
